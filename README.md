9211dee80669fc23f205ad5d8453101da12d5e18
Yuqing <zhong.yuq@husky.neu.edu>
2017-06-12 18:30:46 -0400

    update project from old project
    assignment part has been removed

---
+++ README.md
@@ -0,0 +1,11 @@
+About me: My name is Yuqing Zhong. This is my first year of being a Master
+student in Computer Science. I majored in Psychology when I was an undergraduate.
+
+About the repository: This repository is for the web development class I'm
+taking at Northeastern University. I put all the assignments and projects in
+this repository. You can find them in the /public folder.
+
+Quick links
+a. My hosted environment on Heroku: https://webdev-2017summer.herokuapp.com/    
+b. My project: https://webdev-2017summer.herokuapp.com/project/index.html    
+c. My assignments:https://webdev-2017summer.herokuapp.com/assignment/index.html
--- server.js
+++ server.js
@@ -1,21 +1,17 @@
-/**
- * Created by Deus on 2017/6/7.
- */
 var app = require('./express');
 // var bodyParser = require('body-parser');
 
-// var bodyParser = require('body-parser');
-// app.use(bodyParser.json());// parse the body
-// app.use(bodyParser.urlencoded({extended: true}));
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());// parse the body
+app.use(bodyParser.urlencoded({extended: true}));
 
 // configure a public directory to host static content
 app.use(app.express.static(__dirname + '/public'));
 
-// var ourApp = require('./lectures/angular/app.js');
-// ourApp(app);
 
 // require("./test/app.js")(app);
-require("./public/test/app");
+// require("./assignment/app");
+require("./project/app");
 // require('./public/app.js')();
 // don't know need it or not
 var port = process.env.PORT || 3000;
